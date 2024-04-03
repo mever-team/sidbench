b44350e12942189d02ac37db8922958e7f64af8f
Manos Schinas <manosetro@hotmail.com>
2024-04-03 13:59:54 +0300

    Fix how DIMD is loaded

--- models/DIMD.py
+++ models/DIMD.py
@@ -17,6 +17,11 @@ class DIMD(nn.Module):
         state_dict = torch.load(ckpt, map_location='cpu')
         try:
             self.model.load_state_dict(state_dict['model'])
+            if ('module._conv_stem.weight' in state_dict['model']) or ('module.fc.fc1.weight' in state_dict['model']) or ('module.fc.weight' in state_dict['model']):
+                self.load_state_dict({key[7:]: state_dict['model'][key] for key in state_dict['model']})
+            else:
+                self.model.load_state_dict(state_dict['model'])
+
         except:
             print('Loading state dict failed. Trying to load without model prefix.')
             self.model.load_state_dict(state_dict)
