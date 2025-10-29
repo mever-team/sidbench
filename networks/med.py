980cb34baa86458eb308de25ff61d25af3d527b7
Peter-Fayez95 <peterfayez1285@gmail.com>
2025-10-29 19:11:33 +0400

    fix transformers import

--- networks/med.py
+++ networks/med.py
@@ -36,12 +36,14 @@ from transformers.modeling_outputs import (
     SequenceClassifierOutput,
     TokenClassifierOutput,
 )
-from transformers.modeling_utils import (
-    PreTrainedModel,
+from transformers.modeling_utils import PreTrainedModel
+
+from transformers.pytorch_utils import (
     apply_chunking_to_forward,
     find_pruneable_heads_and_indices,
     prune_linear_layer,
 )
+
 from transformers.utils import logging
 from transformers.models.bert.configuration_bert import BertConfig
 
